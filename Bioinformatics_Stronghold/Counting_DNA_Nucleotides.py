b8b19112b7a90af68106d50c1c128db43678efb6
Erfan <cigano@gmx.com>
2021-02-20 10:10:13 +0000

    Counting DNA Nucleotides

---
+++ Bioinformatics_Stronghold/Counting_DNA_Nucleotides.py
@@ -0,0 +1,24 @@
+# Pass the text file from website to read_txt
+# Then we pass the read_txt(rosalind.txt) as argument to nuc_frequency
+
+from collections import Counter
+
+def read_txt(textfile):
+    """
+    This function reads the text file in the directory. The only input, is the name 
+    of the file.
+    """
+    bases = ["A", "T", "C", "G"]
+    seq = open(textfile, "r")
+    seq_read = seq.readlines()
+    seq.close()
+    return "".join([nuc for nuc in seq_read[0] if nuc in bases])
+
+
+def nuc_frequency(seq):
+    """
+    Given: A DNA string s of length at most 1000 nt.
+    Return: Four integers (separated by spaces) counting the respective number of times that 
+    the symbols 'A', 'C', 'G', and 'T' occur in s.
+    """      
+    return Counter(seq).values()
