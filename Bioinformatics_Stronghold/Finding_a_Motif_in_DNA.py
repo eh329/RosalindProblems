51c1adae547c572200f9d582fd27586e2993a2b3
Erfan Hosseinian Rad <erfanhoseynian@gmail.com>
2025-05-04 18:33:37 +0100

    Update Finding_a_Motif_in_DNA.py

--- Bioinformatics_Stronghold/Finding_a_Motif_in_DNA.py
+++ Bioinformatics_Stronghold/Finding_a_Motif_in_DNA.py
@@ -1,14 +1,26 @@
 # Just pass the text file to this function
 
-def substring(text):
+import re
+
+def read_str(txt):
+    file = open(txt, "r")
+    s, k = [x.strip() for x in file.readlines()]
+    file.close()
+
+    return s, k
+
+def find_motif(s, k):
     """
     Given: Two DNA strings s and t (each of length at most 1 kbp).
     Return: All locations of t as a substring of s.
     """
-    file = open(text, "r")
-    final = file.readlines()
-    string = final[0].split("\n")[0]
-    substring = final[1].split("\n")[0]
-    
-    for number in [index + 1 for index in range(len(string)) if s.startswith(substring, index)]:
-            print(number, end = " ")
+    pattern = re.compile(k)
+    r = pattern.search(s)
+
+    if not r: 
+        print(-1)
+        
+    while r:
+        indecis = r.start() 
+        print(indecis + 1, end = " ")
+        r = pattern.search(s, r.start() + 1)
